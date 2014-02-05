e45eb5197909e8b0735f58e6644abed2262f56ed
Christoffer Maarup Noerbjerg <christoffernoerbjerg@me.com>
2014-02-05 11:21:38 +0100

    Exercise 6.2

--- dDistEx6.2/src/EchoServer.java
+++ dDistEx6.2/src/EchoServer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.net.InetAddress;
 import java.rmi.registry.*;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
@@ -20,7 +21,8 @@ public class EchoServer extends UnicastRemoteObject implements Echo {
 
     public static void main(String[] args) throws IOException {
         EchoServer server = new EchoServer();
-        Runtime.getRuntime().exec("rmiregistry 1099");
+        System.out.println(InetAddress.getLocalHost());
+                Runtime.getRuntime().exec("rmiregistry 1099");
         LocateRegistry.createRegistry(1099);
 
         Naming.rebind("//localhost:" + portNumber + "/dDistEcho", server);

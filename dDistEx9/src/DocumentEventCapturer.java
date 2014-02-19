535cfa00b74ad6f206755c93e41969101adeb489
Niels <houmandk@gmail.com>
2014-02-19 10:45:00 +0100

    This should fix Ex9

--- dDistEx9/src/DocumentEventCapturer.java
+++ dDistEx9/src/DocumentEventCapturer.java
@@ -32,7 +32,7 @@ public class DocumentEventCapturer extends DocumentFilter {
      *
      * @return Head of the recorded event queue. 
      */
-    MyTextEvent take() throws InterruptedException {
+    public MyTextEvent take() throws InterruptedException {
         return eventHistory.take();
     }
 
